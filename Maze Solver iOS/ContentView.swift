ae84ec9944a760121c524e4aed781f6386cc4d16
Luke <hawadlu@outlook.com>
2021-01-05 13:10:36 +1300

    Initial Commit

---
+++ Maze Solver iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Maze Solver iOS
+//
+//  Created by Luke Adam Hawinkels on 5/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Maze Solver iOS/Maze_Solver_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Maze_Solver_iOSApp.swift
+//  Maze Solver iOS
+//
+//  Created by Luke Adam Hawinkels on 5/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Maze_Solver_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
